09084a6a3adb0708f7b710ee0519c83aefa9c851
Sharthak Sharma <sharthaksharma.ss@gmail.com>
2024-04-24 15:34:41 +0530

    [feat] : add rgb thresholding

--- filters.js
+++ filters.js
@@ -150,15 +150,16 @@ const powerTransform = ({ data }, gamma) => {
 
 const thresholding = ({ data }, threshold) => {
   if (!threshold) threshold = 128;
-  blackAndWhite({ data });
 
   for (let i = 0; i < data.length; i += 4) {
-    let val = data[i];
-    val = 255 * (val >= threshold);
+    const grayLevel =
+      0.299 * data[i] + 0.587 * data[i + 1] + 0.114 * data[i + 2];
 
-    data[i] = val;
-    data[i + 1] = val;
-    data[i + 2] = val;
+    if (grayLevel < threshold) {
+      data[i] = 0;
+      data[i + 1] = 0;
+      data[i + 2] = 0;
+    }
   }
 };
 
